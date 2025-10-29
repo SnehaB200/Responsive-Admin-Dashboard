c438420d69e0c7860605519ef237f293abe581aa
Snehangi Behera <115468014+SnehaB200@users.noreply.github.com>
2025-10-29 18:31:10 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,39 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber :'JS101',
+        paymentStatus :'Due',
+        status: "Pending"
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber :'CFC103',
+        paymentStatus :'Refund',
+        status: "Declined"
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber :'FBT1105',
+        paymentStatus :'Paid',
+        status: "Active"
+    },
+    // {
+    //     productName: 'Frontend Development Tutorial',
+    //     productNumber :'FDT110',
+    //     paymentStatus :'Paid',
+    //     status: "Active"
+    // },
+    // {
+    //     productName: 'Backend Development Tutorial',
+    //     productNumber :'BDT120',
+    //     paymentStatus :'Refund',
+    //     status: "Declined"
+    // },
+    // {
+    //     productName: 'Full Stack Developer Tutorial',
+    //     productNumber :'FSDT130',
+    //     paymentStatus :'Due',
+    //     status: "Pending"
+    // }
+
+]
